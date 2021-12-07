16a6f71b611b1ab52f0ab02c715bcb51761533d3
xavieraiden <45552520+xavieraiden@users.noreply.github.com>
2021-12-07 10:27:07 +1000

    Racist chinese sentence maker

---
+++ racist.py
@@ -0,0 +1,17 @@
+# please ignore all the dumbass spelling mistakes
+
+# imports random
+import random
+# establishes the racist syllables used in the sentences
+racism = ["ching","chong","ling",'xi','cha','hao','bing','wing','long']
+# input to specify how many syllables in generated sentence
+amnt = input("How many syllables? ")
+# idek if i needed to do this but it's just and empty string
+sentence = ""
+# adds together random words from racism until it reaches specified length.
+for x in range(int(amnt)):
+    sentence = sentence + " " + random.choice(racism)
+# outputs sentence
+print(sentence)
+# pauses until input so you can read your sentence
+input("Press literally anything to make this go away, preferrably forever")
